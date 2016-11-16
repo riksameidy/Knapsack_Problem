c22fda846efa11b21cd7a06a79d7191da8e38aa9
riksameidy <riksameidy@gmail.com>
2016-11-16 20:38:13 +0700

    edit Driver + Tubes

--- Driver.java
+++ Driver.java
@@ -1,27 +1,50 @@
 import java.util.Scanner;
 
 public class Driver {
-  public static void main(String[] args) {
-    Tubes tubes = new Tubes();
-    Scanner s = new Scanner();
+  public static void main(String[] args)
+    Scanner s = new Scanner(System.in);
+    Tubes knapsack;
 
     boolean menu = true;
     while(menu){
       System.out.println("Kasus 0/1 Knapsack");
-      System.out.println("1. Solve dengan Brute Force");
-      System.out.println("2. Solve dengan Backtrack");
-      System.out.println("3. Solve dengan Branch And Bound");
+      System.out.println("1. Edit List Barang");
+      System.out.println("2. Lihat List Barang");
+      System.out.println("3. Solve dengan Brute Force");
+      System.out.println("4. Solve dengan Backtrack");
+      System.out.println("5. Solve dengan Branch And Bound");
+      System.out.println("6.Exit");
       int pil = s.nextInt();
       switch (pil) {
         default:
           break;
         case 1:
+          System.out.println("Masukan Kapasitas Knapsack");
+          int k = s.nextInt();
+          if(k<4 || k>20){
+            System.out.println("Input Harus Minimal 4 dan Maksimal 20");
+            break;
+          }
+          knapsack = new Tubes(k);
+          System.out.println("Masukan Jumlah Barang");
+          int jum = s.nextInt();
+          for (int i=0; i<jum;i++ ) {
+            System.out.println("=========== Barang " + (i+1) + "============");
+            System.out.print("Masukkan Weight: ");
+            int w = s.nextInt();
+            System.out.print("Masukan Value: ");
+            int v = s.nextInt();
+            knapsack.addBarang(w,v);
+            System.out.println("=============================================");
+          }
           break;
         case 2:
           break;
         case 3:
           break;
         case 4:
+          break;
+        case 5:
           menu = false;
           break;
       }
--- Tubes.java
+++ Tubes.java
@@ -1,14 +1,27 @@
 public class Tubes {
 
-
       private ArrayList<Barang> barang;
-      
-      public Tubes(){
+      private int kapasitas;
+
+      public Tubes(int kapasitas){
         barang = new ArrayList<>();
+        this.kapasitas = kapasitas;
+      }
+
+      public void addBarang(int w , int v){
+        barang.add(new Barang(w,v));
+      }
+
+      public int getKapasitas(){
+        return kapasitas;
+      }
+
+      public ArrayList<Barang> getBarang(){
+        return barang;
       }
 
       public void bruteForce(){
-
+        
       }
 
       public void backtrack(){
